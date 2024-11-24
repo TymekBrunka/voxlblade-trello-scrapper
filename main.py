2b2d4f02f1fe0ce3420b7c2b9d20db60d3dbbeb0
TymekBrunka <tymkopolo@wp.pl>
2024-11-24 20:05:54 +0100

    should work i guess

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import os
 from trello import TrelloClient
 from dotenv import load_dotenv, dotenv_values 
 import re
+import json
 load_dotenv() 
-
-print()
 
 # Initialize the Trello client
 client = TrelloClient(
@@ -13,27 +12,40 @@ client = TrelloClient(
     token=os.getenv("TOKEN"),  # Optional for public boards
 )
 
+data = {}
 # Get a public board by its ID or URL
 board = client.get_board('/XyvHrkbi')
 
 # Access lists and cards
 for lst in board.list_lists():
     print(f'List: {lst.name}')
+    data[lst.name] = {}
     for card in lst.list_cards():
         print(f'  Card: {card.name}')
-        print(f'    labels: {[label.name for label in card.labels]}')
+        data[lst.name][card.name] = {}
+        # labels = [label.name for label in card.labels]
+        # data[lst.name][card.name]["labels"] = labels
+        data[lst.name][card.name]["labels"] = [label.name for label in card.labels]
+        # print(f'    labels: {labels}')
+        data[lst.name][card.name]["description"] = card.description;
         # print(f'    info: {card.description}')
-        # print(f'    attachments: {card.attachments}')
         for attachment in card.attachments:
-            "".startswith("https://trello.com/c/")
             if attachment["url"].startswith("https://trello.com/c/"):
                 match = re.search(r'/c/([^/]+)', attachment["url"]);
                 card_id = ""
                 if match and attachment['url'] != card.shortUrl:
+                    data[lst.name][card.name]["attachments"] = []
                     card_id = match.group(1)
                     new_card = client.get_card(card_id)
                     print("    attached card:")
+                    # print(f"    on list: {new_card.get_list().name}")
                     print(f'      Card: {new_card.name}')
-                    print(f'        labels: {[label.name for label in card.labels]}')
+                    data[lst.name][card.name]["attachments"].append(f"{new_card.get_list().name}.{new_card.name}")
+                    # print(f'        labels: {[label.name for label in card.labels]}')
                 else:
-                    print(f"    failed attached card: {attachment['name']}")
+                    # print(f"    failed attached card: {attachment['name']}")
+                    pass
+
+print("\n\n\n\n")
+with open("./voxl-dumped.json", "w") as f:
+    f.write(json.dumps(data, indent=2));
